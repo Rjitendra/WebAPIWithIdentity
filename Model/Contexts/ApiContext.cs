331cc7040a0d8fb103bfb063599925246ce63c7b
Jitendra <jbehera@bridgetree.com>
2019-09-19 20:13:24 +0530

    api setup

---
+++ Model/Contexts/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Model.Entity;
+
+namespace Model.Contexts
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        // Rest of the code
+    }
+}
---
+++ Model/Entity/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model.Entity
+{
+    [Table("Client", Schema = "dbo")]
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public DateTimeOffset DOB { get; set; }
+        public bool Gender { get; set; }
+        public string Address { get; set; }
+    }
+}
